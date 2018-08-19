c86ba86237802a020a9ae4da4c1de5476dd33b5e
Paul Bailey <paul.m.bailey@gmail.com>
2018-08-18 17:16:45 -0700

    setup frontend

--- src/App.js
+++ src/App.js
@@ -2,17 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.backend = process.env.REACT_APP_BACKEND_URL;
+    this.state = {
+      zip: '',
+      weather: ''
+    };
+  }
+
+  update_zip(event) {
+    this.setState({zip: event.target.value});
+  }
+
+  lookup(event) {
+    event.preventDefault();
+    axios
+      .get(this.backend + '/?zip=' + this.state.zip)
+      .then((result) => {
+        console.log(result.data);
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">DigitalCrafts Weather</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br/><br/>
+        <form className="App-intro" onSubmit={(e) => this.lookup(e)}>
+          <label>Zip Code</label>
+          <br/>
+          <input type="zip" onChange={(e) => this.update_zip(e)} maxLength="5"/>
+          <br/><br/>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
